caa917948b045f9b5b4c0c70026000df417207fa
zshnrg <ghosanirozan1@gmail.com>
2024-11-30 13:43:19 +0700

    Feat: Working scanner on web

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
